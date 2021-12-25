4ac1d2f9b8b296b6dcb2131a015eaece47752148
jittusaroj <43810688+jittusaroj@users.noreply.github.com>
2021-12-26 05:06:06 +0530

    astrologer profile

--- src/screens/Astrologer.js
+++ src/screens/Astrologer.js
@@ -1,63 +1,96 @@
 import React, { component, useState } from 'react';
 import { StyleSheet, Text, View, ImageBackground, Image, ScrollView } from 'react-native';
-import { TextInput, Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
-const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
-import { Tabs, TabScreen, useTabIndex, useTabNavigation } from 'react-native-paper-tabs';
+import { TextInput, Avatar, Button, Card, Title, Paragraph, IconButton } from 'react-native-paper';
 const Astrologer = () => {
-    
+
 
     return (
         <>
-            <Card style={{ backgroundColor: "white" }}>
-                <Card.Content>
-                    <Image style={styles.tinyLogo} source={require('../../assets/horoscope.jpg')}
-                    />
-                    <Title style={{textAlign:"center",marginBottom:10}} >Mesh</Title>
-                </Card.Content>
-            </Card>
-            <ScrollView>
-                <Tabs>
-                    <TabScreen label="Today">
-                        <View style={{ position: "absolute", left: 0, right: 0, bottom: 0 }}>
-                            <Text>this is airplan tab view</Text>
-                            <Text>this is airplan tab view</Text>
+            <View>
+                <ImageBackground source={require('../../assets/astrobg.jpg')} style={{ width: '100%', height: 280, }}>
+                    <View style={styles.flex}>
+                        <View>
+                            <Image style={styles.icon} source={require('../../assets/horoscope.jpg')} />
+                        </View>
+                        <View style={styles.ml5}>
+                            <Title style={{ color: "white" }} >Know your destiny</Title>
+                            <Paragraph style={{ color: "white" }}>Select your zodiac-sign</Paragraph>
+                        </View>
+                    </View>
+                </ImageBackground>
 
-                        </View>
-                    </TabScreen>
-                    <TabScreen label="Tomorrow" >
-                        <View style={{ backgroundColor: 'red' }} >
-                            <Text>this is airplan tab view</Text>
-                        </View>
-                    </TabScreen>
-                </Tabs>
+            </View>
+
+            <ScrollView>
                 <Card>
                     <Card.Content>
-                        <Title>24/10/2022</Title>
-                        <Paragraph>
-                            Proident culpa quis exercitation aute proident eiusmod elit.
-                            Proident culpa quis exercitation aute proident eiusmod elit.
-                            Proident culpa quis exercitation aute proident eiusmod elit.
-                            Proident culpa quis exercitation aute proident eiusmod elit.
-                            Proident culpa quis exercitation aute proident eiusmod elit.
-                        </Paragraph>
-                        <Paragraph>
-                            Proident culpa quis exercitation aute proident eiusmod elit.
-                            Proident culpa quis exercitation aute proident eiusmod elit.
-                            Proident culpa quis exercitation aute proident eiusmod elit.
-                            Proident culpa quis exercitation aute proident eiusmod elit.
-                            Proident culpa quis exercitation aute proident eiusmod elit.
-                        </Paragraph>
+                        <Title>Experience</Title>
+                        <Paragraph>12 Year</Paragraph>
+                        <Title>Consultation charge</Title>
+                        <Paragraph>Rs. 300/min </Paragraph>
+                        <Title>Location</Title>
+                        <Paragraph>New Delhi </Paragraph>
+                        <Title>Language spoken</Title>
+                        <Paragraph>Hindi, English </Paragraph>
+                        <Title>Expertise</Title>
+                        <Paragraph>Vedic, Prashna kundali </Paragraph>
+                        <Title>profile summery</Title>
+                        <Paragraph>Aliquip veniam dolor est laborum non esse dolor tempor officia id.Aliquip veniam dolor est laborum non esse dolor tempor officia idAliquip veniam dolor est laborum non esse dolor tempor officia idAliquip veniam dolor est laborum non esse dolor tempor officia idAliquip veniam dolor est laborum non esse dolor tempor officia idAliquip veniam dolor est laborum non esse dolor tempor officia idAliquip veniam dolor est laborum non esse dolor tempor officia
+                            Aliquip veniam dolor est laborum non esse dolor tempor officia id.Aliquip veniam dolor est laborum non esse dolor tempor officia idAliquip veniam dolor est laborum non esse dolor tempor officia idAliquip veniam dolor est laborum non esse dolor tempor officia idAliquip veniam dolor est laborum non esse dolor tempor officia idAliquip veniam dolor est laborum non esse dolor tempor officia idAliquip veniam dolor est laborum non esse dolor tempor officia id </Paragraph>
                     </Card.Content>
                 </Card>
+
             </ScrollView>
+            <View style={{ flexDirection: "row" }}>
+                <Button style={{ flex: 1, borderRadius: 0, padding: 10, fontSize: 15 }} icon="message">Chat now</Button>
+                <Button mode="contained" style={{ flex: 1, borderRadius: 0, padding: 10, fontSize: 15 }} icon="phone">Call now</Button>
+            </View>
+
         </>
     );
 }
 export default Astrologer
 
 
+
+
 const styles = StyleSheet.create({
-    
+    container: {
+        paddingTop: 50,
+
+    },
+    mt4: {
+        marginTop: 10
+    },
+    center: {
+        textAlign: "center",
+        fontSize: 18,
+        fontWeight: "900"
+    },
+    small: {
+        textAlign: "center",
+        fontSize: 13,
+    },
+    leftAlign: {
+        textAlign: "right",
+        marginBottom: 30
+    },
+    bgTransparent: {
+        backgroundColor: "transparent",
+        shadowColor: "transparent"
+    },
+    formControl: {
+        marginBottom: 15,
+
+        backgroundColor: "transparent"
+    },
+    flex: {
+        flex: 1,
+        alignItems: "center",
+        flexDirection: "row",
+
+
+    },
     tinyLogo: {
         width: 150,
         height: 150,
@@ -65,5 +98,20 @@ const styles = StyleSheet.create({
         marginTop: 70,
         alignSelf: 'center',
     },
-    
+    icon: {
+        width: 70,
+        height: 70,
+        borderRadius: 50,
+        marginLeft: 5
+    },
+    logo: {
+        width: 66,
+        height: 58,
+    },
+    ml5: {
+        marginLeft: 15,
+    },
+    mb5: {
+        marginBottom: 5,
+    },
 });
